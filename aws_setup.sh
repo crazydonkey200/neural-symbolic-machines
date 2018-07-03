75cdfc48d5b27194f4a35b2da91564ecec8771a4
crazydonkey200 <crazydonkey200@gmail.com>
2018-07-03 02:02:57 -0700

    Bugfix when downloading data.

--- aws_setup.sh
+++ aws_setup.sh
@@ -2,7 +2,8 @@ source activate tensorflow_p27
 pip install -r requirements.txt
 mkdir ~/projects/data/
 cd ~/projects/data/
-wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=104DnlcOHc5r60-PkQOyc_mcvLSs630Sl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=104DnlcOHc5r60-PkQOyc_mcvLSs630Sl" -O wikitable.zip && rm -rf /tmp/cookies.txtunzip wikitable.zip
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=104DnlcOHc5r60-PkQOyc_mcvLSs630Sl' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=104DnlcOHc5r60-PkQOyc_mcvLSs630Sl" -O wikitable.zip && rm -rf /tmp/cookies.txt
+unzip wikitable.zip
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1lwLH4-5FRZzM9JVicy3TH6Al11bRalyg' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1lwLH4-5FRZzM9JVicy3TH6Al11bRalyg" -O wikisql.zip && rm -rf /tmp/cookies.txt
 unzip wikisql.zip
 cd ~/projects/neural-symbolic-machines/
