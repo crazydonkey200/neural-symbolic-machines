09e5b39172d1fc93cf1cbae6d45976d35106a38d
Ubuntu <ubuntu@ip-172-31-36-64.us-west-2.compute.internal>
2018-07-04 03:49:08 +0000

    update exploration code.

--- table/wtq/explore.sh
+++ table/wtq/explore.sh
@@ -1,6 +1,6 @@
 ROOT=$HOME"/projects/"
 DATA_DIR=$ROOT"data/wikitable/"
-python mp_random_explore.py \
+python ../random_explore.py \
        --train_file_tmpl=$DATA_DIR"processed_input/preprocess_14/data_split_1/train_split_shard_90-{}.jsonl" \
        --table_file=$DATA_DIR"processed_input/preprocess_14/tables.jsonl" \
        --output_dir=$DATA_DIR"output" \
@@ -9,5 +9,5 @@ python mp_random_explore.py \
        --save_every_n=5 \
        --n_epoch=200 \
        --id_start=0 \
-       --id_end=1 \
+       --id_end=90 \
        --alsologtostderr
