527c7f446e819518d3f6f790f14349c80dd4d55e
crazydonkey200 <crazydonkey200@gmail.com>
2018-08-08 00:47:49 -0700

    Bugfix.

--- table/wtq/preprocess.sh
+++ table/wtq/preprocess.sh
@@ -1,6 +1,6 @@
 ROOT=$HOME"/projects/"
 DATA_DIR=$ROOT"data/wikitable/"
-python wtq_preprocess.py \
+python preprocess.py \
        --raw_input_dir=$DATA_DIR"raw_input" \
        --processed_input_dir=$DATA_DIR"processed_input/wtq_preprocess" \
        --max_n_tokens_for_num_prop=10 \
